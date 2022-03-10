2129a88b92fd9363e325326887b886761e407f5a
NiamhGillespie <2549880G@student.gla.ac.uk>
2022-03-10 17:24:28 +0000

    sign up fixed

--- rango/views.py
+++ rango/views.py
@@ -68,21 +68,13 @@ def sign_up(request):
     
     if request.method == 'POST':
         user_form = UserForm(request.POST)
-        profile_form = UserProfileForm(request.POST)
         
-        if user_form.is_valid() and profile_form.is_valid():
+        if user_form.is_valid():
             user = user_form.save()
             
             user.set_password(user.password)
             user.save()
-            
-            profile = profile_form.save(commit = False)
-            profile.user = user
-            
-            if 'picture' in request.FILES:
-                profile.picture = request.FILES['picture']
-                
-            profile.save()
+           
             registered = True
             
         else:
@@ -90,11 +82,9 @@ def sign_up(request):
     
     else:
         user_form = UserForm()
-        profile_form = UserProfileForm()
         
     return render(request, 'rango/signup.html',
             context = {'user_form' : user_form,
-                       'profile_form' : profile_form,
                        'registered' : registered})
         
         
