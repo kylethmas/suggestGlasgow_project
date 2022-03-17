c4eea4681522e5cdd46dbea98231691f5fc10f40
Kyle T <2548971t@student.gla.ac.uk>
2022-03-17 11:18:10 +0000

    home page

--- rango/urls.py
+++ rango/urls.py
@@ -7,7 +7,7 @@ app_name = 'suggestGlasgow'
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('', views.index, name='index'),
+    path('Index', views.index, name='index'),
     path('category/<slug:category_name_slug>/', views.show_category, name = 'show_category'),
     #path('add_category/', views.add_category, name='add_category'),
     #path('category/<slug:category_name_slug>/add_page/', views.add_page, name='add_page'),
