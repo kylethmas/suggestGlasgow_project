521c5fb434bd5b5c8a2155ba5e97b2a63164ab88
NiamhGillespie <2549880G@student.gla.ac.uk>
2022-03-24 17:25:24 +0000

    stashing changes

--- rango/views.py
+++ rango/views.py
@@ -91,25 +91,26 @@ def add_place(request):
     if request.method == 'POST':
         form = PlaceForm(request.POST)
 
-    if form.is_valid():
-        form.save()
+        if form.is_valid():
+            form.save()
 
-        print("Page has been saved!!!")
-        name = form['place_name'].value()
-        place = Place.objects.get(place_name=name)
-        print(place)
-        return redirect(reverse('suggestGlasgow:show_place',
-                                    kwargs={'place_name_slug':
-                                                place.slug}))
-        #return redirect(reverse('suggestGlasgow:show_place',
-         #                       kwargs={'place_name_slug':
-          #                                  place.slug}))
+            print("Page has been saved!!!")
+            name = form['place_name'].value()
+            place = Place.objects.get(place_name=name)
+            print(place)
+            return redirect(reverse('suggestGlasgow:home'))
+            #return redirect(reverse('suggestGlasgow:show_place',
+             #                           kwargs={'place_name_slug':
+              #                                      place.slug}))
+            #return redirect(reverse('suggestGlasgow:show_place',
+             #                       kwargs={'place_name_slug':
+              #                                  place.slug}))
 
-    else:
-        print(form.errors)
-        print("Page has wrong!!!")
-        print(form.errors)
-        #return redirect('/')
+        else:
+            print(form.errors)
+            print("Page has wrong!!!")
+            print(form.errors)
+            #return redirect('/')
 
     context_dict = {'form': form}
     return render(request, 'rango/add_place.html', context=context_dict)
