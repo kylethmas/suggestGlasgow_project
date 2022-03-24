c90b955febda76da03410b918bc40bbd21eab420
Kyle T <2548971t@student.gla.ac.uk>
2022-03-24 18:08:05 +0000

    setting

--- suggestGlasgow_project/settings.py
+++ suggestGlasgow_project/settings.py
@@ -19,7 +19,8 @@ STATIC_DIR = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [STATIC_DIR, ]
 MEDIA_DIR = os.path.join(BASE_DIR, 'media')
 ALLOWED_HOSTS = ['https://kylethmas.pythonanywhere.com/',
-                 'host 127.0.0.1']
+                 'host 127.0.0.1',
+                'kylethmas.pythonanywhere.com']
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
