0feb281b43cc623f5f42a4b02a2fb879b63ddee5
Luka Senic <2619426s@student.gla.ac.uk>
2022-03-25 13:50:09 +0000

    Fixed home page layout, added nice animation

---
+++ static/js/Home.mjs
@@ -0,0 +1,31 @@
+function* ChangeText(){
+    const SelectTypeMenu = document.querySelector("#SelectTypeMenu");
+    const AnimatedItem = SelectTypeMenu.querySelector(":scope > div > div:first-child");
+    const Options = [...SelectTypeMenu.querySelectorAll(":scope > div > div:not(:first-child)")].map(Element => Element.innerText);
+    let CurrentIndex = 0;
+    while(true){
+        const CurrentString = Options[(CurrentIndex++) % Options.length];
+        for(let i = 0; i < CurrentString.length; ++i){
+            AnimatedItem.innerText += CurrentString[i];
+            yield Math.random() * 150. + 150.;
+        }
+        yield 1500.;
+        for(let i = 0; i < CurrentString.length; ++i){
+            AnimatedItem.innerText = AnimatedItem.innerText.slice(0, -1);
+            yield 80.;
+        }
+        yield 600.;
+    }
+}
+
+const ChangeTextGen = ChangeText();
+void function Animate(){
+    window.setTimeout(Animate, ChangeTextGen.next().value);
+}();
+
+SelectTypeMenu.addEventListener("click", function(Event){
+    const Type = Event.target.dataset.type;
+    if(Type === undefined) return;
+    document.querySelector("#id_place_type").value = Event.target.innerText;
+    document.querySelector("#SubmitButton").click(); //Submit form
+});
