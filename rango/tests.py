aeaf7d168cac489c753a7208f9cc26c2aeacdad2
NiamhGillespie <2549880G@student.gla.ac.uk>
2022-03-26 17:06:35 +0000

    stashing changes

--- rango/tests.py
+++ rango/tests.py
@@ -84,39 +84,36 @@ class PlaceMethodTests(TestCase):
         place.save()
         self.assertEqual(place.url, "https://moodle.gla.ac.uk/course/view.php?id=29970")
     
-    """ not working :) 
-    def test_default_likes_creation(self):
-        "" "
+     
+    def test_str_method(self):
+        """
+        Checks to make sure the __str__ method works.
+        """
+        place = Place(place_name = "Random Place String")
+        place.save()
+        self.assertEqual(str(place), "Random Place String")
+        
+    def test_number_of_likes_method(self):
+        """
         Checks to make sure that when a place is created, an
         default number of likes is created.
         Example: likes should be ???.
-        "" "
-        place = Place()
+        """
+        place = Place(place_name = "Random Place String")
         place.save()
-        self.assertEqual(place.likes, [])
-    """
-    
-    """ not working :) 
-    def test_default_dislikes_creation(self):
-        "" "
+        self.assertEqual(place.number_of_likes(), 0)
+        
+        
+    def test_number_of_dislikes_method(self):
+        """
         Checks to make sure that when a place is created, an
-        default number of dislikes is created.
-        Example: dislikes should be ???.
-        "" "
-        place = Place()
+        default number of likes is created.
+        Example: likes should be ???.
+        """
+        place = Place(place_name = "Random Place String")
         place.save()
-        self.assertEqual(place.dislikes, [])
-    """
-    """
-    def example_place(name, place_type, place_image, latitude, longitude, url):
-        place = Place.objects.get_or_create(place_name=name, place_type = place_type)[0]
-        place.place_image = place_image
-        place.latitude = latitude
-        place.longitude = longitude
-        place.url = url
-        place.save()
-        return place
-    """
+        self.assertEqual(place.number_of_dislikes(), 0)
+    
 
 
 class UserMethodTests(TestCase):
@@ -144,31 +141,105 @@ class UserMethodTests(TestCase):
         
         self.assertEqual(user.email, "n@gmail.com")
     
-    """ same problem as before
+    """
     def test_password_creation(self):
         "" "
         Checks to make sure that when a user is created, an
         appropriate saves list is created.
         Example: userProfile.saves should be null.
         "" "
-        #user = add_user('Niamh', "n@gmail.com", "thisisabadpassword" )
         user = User.objects.create_user('Niamh', "n@gmail.com", "thisisabadpassword")
         u = UserProfile.objects.get_or_create(user=user)[0]
         u.save()
         
-        self.assertEqual(UserProfile.saves, None)
+        self.assertEqual(u.number_of_saves, 0) #why istn this working
     """
 
 class HomeViewTests(TestCase):
     def test_if_drop_down_categories_are_present(self):
         """
-        If no categories exist, the appropriate message should be displayed.
+        Check if the categories are displaying properly
         """
         response = self.client.get(reverse('suggestGlasgow:home'))
-        print(response)
+        
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, 'Restaurant')
         self.assertContains(response, 'Cafe')
         self.assertContains(response, 'Fast Food')
         self.assertContains(response, 'Nightlife')
         
+    def test_if_header_works(self):
+        """
+        Check if the header is displaying properly
+        """
+        response = self.client.get(reverse('suggestGlasgow:home'))
+        
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'suggestGlasgow')
+        
+    def test_if_footer_works(self):
+        """
+        Check if the footer is displaying properly
+        """
+        response = self.client.get(reverse('suggestGlasgow:home'))
+        
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'Contact us!')
+        
+class LogInViewTests(TestCase):
+    def test_login_box_working(self):
+        """
+        Check if the log in box is working
+        """
+        response = self.client.get(reverse('suggestGlasgow:login'))
+        
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'Log in to SuggestGlasgow')
+        self.assertContains(response, 'Username')
+        self.assertContains(response, 'Password')
+        self.assertContains(response, 'Log in')
+        self.assertContains(response, 'Go back')
+        self.assertContains(response, "Don't have an account? Sign up now!")
+        
+class SignUpViewTests(TestCase):
+    def test_signup_box_working(self):
+        """
+        Check if the log in box is working
+        """
+        response = self.client.get(reverse('suggestGlasgow:sign up'))
+
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'Sign up to SuggestGlasgow')
+        self.assertContains(response, 'Username')
+        self.assertContains(response, 'Password')
+        self.assertContains(response, 'Email')
+        self.assertContains(response, 'Log in')
+        self.assertContains(response, 'Go back')
+        self.assertContains(response, "Already have an account? Log in!")
+        
+class PlacesViewTests(TestCase):
+    def test_basic_view(self):
+        """
+        Check if the log in box is working
+        """
+        place = Place(place_name = "Niamh testing cafe", place_type = "Cafe", place_image = "Testing.png", latitude = '55.46', longitude = '4.46', url= 'https://moodle.gla.ac.uk/course/view.php?id=29970')
+        place.save()
+        response = self.client.get(reverse('suggestGlasgow:show_place', kwargs={'place_name_slug': place.slug}))
+
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "Niamh testing cafe")
+        self.assertContains(response, 'Cafe')
+        self.assertContains(response, 'Testing.png')
+        self.assertContains(response, '55.46')
+        self.assertContains(response, '4.46')
+        self.assertContains(response, 'https://moodle.gla.ac.uk/course/view.php?id=29970')
+        self.assertContains(response,  "Click here to visit their website")
+       
+    def test_likes_and_dislikes(self):
+        place = Place(place_name = "Niamh testing cafe", place_type = "Cafe", place_image = "Testing.png", latitude = '55.46', longitude = '4.46', url= 'https://moodle.gla.ac.uk/course/view.php?id=29970')
+        place.save()
+        response = self.client.get(reverse('suggestGlasgow:show_place', kwargs={'place_name_slug': place.slug}))
+        
+        self.assertContains(response, place.likes.count())
+        self.assertContains(response, place.dislikes.count())
+   
