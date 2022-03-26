eb43b6f65d2fc46f7d7c9f704ea17170bbf6b30c
Kyle T <2548971t@student.gla.ac.uk>
2022-03-26 20:29:31 +0000

    removed unused code

--- rango/admin.py
+++ rango/admin.py
@@ -1,11 +1,10 @@
 from django.contrib import admin
-from rango.models import Ratings, Place, UserProfile
+from rango.models import Place, UserProfile
 
 
 class PlaceAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug':('place_name',)}
 
 
-admin.site.register(Ratings)
 admin.site.register(Place,PlaceAdmin)
 admin.site.register(UserProfile)
